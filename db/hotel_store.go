8e478347fba76c6f89fcf1dce4e28596b1b53040
Matthew Murdock <moryakovpirat@gmail.com>
2025-02-08 22:29:49 +0300

    little update of stores and seed

--- db/hotel_store.go
+++ db/hotel_store.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"hotel-reservation/types"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type HotelStore interface {
 	InsertHotel(context.Context, *types.Hotel) (types.Hotel, error)
+	Update(context.Context, bson.M, bson.M) error
 }
 
 type MongoHotelStore struct {
@@ -17,11 +19,17 @@ type MongoHotelStore struct {
 	coll   *mongo.Collection
 }
 
-func NewMongoHotelStore(client *mongo.Client, dbname string) *MongoHotelStore {
+func NewMongoHotelStore(client *mongo.Client) *MongoHotelStore {
 	return &MongoHotelStore{
 		client: client,
-		coll:   client.Database(dbname).Collection("hotels"),
+		coll:   client.Database(DBNAME).Collection("hotels"),
 	}
+}
+
+func (s *MongoHotelStore) Update(ctx context.Context, filter bson.M, update bson.M) error {
+	_, err := s.coll.UpdateOne(ctx, filter, update)
+	return err
+
 }
 
 func (s *MongoHotelStore) InsertHotel(ctx context.Context, hotel *types.Hotel) (types.Hotel, error) {
--- scripts/seed.go
+++ scripts/seed.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"hotel-reservation/db"
 	"hotel-reservation/types"
 	"log"
@@ -11,18 +12,47 @@ import (
 )
 
 func main() {
+	ctx := context.Background()
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
 	if err != nil {
 		log.Fatal(err)
 	}
-	db := db.NewMongoHotelStore(client, db.DBNAME)
+	hotelStore := db.NewMongoHotelStore(client)
+	roomStore := db.NewMongoRoomStore(client, hotelStore)
 	hotel := types.Hotel{
 		Name:     "Bellucia",
 		Location: "France",
 	}
 
-	room := types.Room{
-		Type:      types.SingleRoomType,
-		BasePrice: 99.9,
+	rooms := []types.Room{
+		{
+			Type:      types.SingleRoomType,
+			BasePrice: 99.9,
+		},
+		{
+			Type:      types.DeluxeRoomType,
+			BasePrice: 199.9,
+		},
+		{
+			Type:      types.SeaSideRoomType,
+			BasePrice: 122.9,
+		},
 	}
+
+	InsertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, room := range rooms {
+		room.HotelID = InsertedHotel.ID
+		InsertedRoom, err := roomStore.InsertRoom(ctx, &room)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(InsertedRoom)
+	}
+
+	fmt.Println(InsertedHotel)
+
 }
